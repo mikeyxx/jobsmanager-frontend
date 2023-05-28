5ff21c737a8ae9b2289deeef0d9e9efd838e3aba
Michael Ogbo <79487718+mikeyxx@users.noreply.github.com>
2023-05-28 01:11:23 +0100

    Update job.tsx

--- src/api/job.tsx
+++ src/api/job.tsx
@@ -2,16 +2,16 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const createJob = async (jobData: any) => {
-  return await axios.post("http://localhost:4000/api/job/create", jobData);
+  return await axios.post("/api/job/create", jobData);
 };
 
 export const updateJob = async (jobData: any, id: string | undefined) => {
   return await axios.patch(
-    `http://localhost:4000/api/job/update/${id}`,
+    `/api/job/update/${id}`,
     jobData
   );
 };
 
 export const deleteJob = async (id: string | undefined) => {
-  return await axios.delete(`http://localhost:4000/api/job/${id}`);
+  return await axios.delete(`/api/job/${id}`);
 };
