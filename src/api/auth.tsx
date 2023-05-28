827f10d611a527d58e90fea0ef9baabcc3bc0102
Michael Ogbo <79487718+mikeyxx@users.noreply.github.com>
2023-05-28 01:10:40 +0100

    Update auth.tsx

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -3,15 +3,15 @@ axios.defaults.withCredentials = true;
 
 export const onRegistration = async (registrationData: any) => {
   return await axios.post(
-    "http://localhost:4000/api/register",
+    "/api/register",
     registrationData
   );
 };
 
 export const onLogin = async (loginData: any) => {
-  return await axios.post("http://localhost:4000/api/login", loginData);
+  return await axios.post("/api/login", loginData);
 };
 
 export const onLogout = async () => {
-  return await axios.get("http://localhost:4000/api/logout");
+  return await axios.get("/api/logout");
 };
